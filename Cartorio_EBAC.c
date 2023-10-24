9c452ad5f98ab5f6221a2c67ff3a160b6a16622f
VandersonBezerra <148904422+VandersonBezerra@users.noreply.github.com>
2023-10-24 16:00:19 -0300

    Upload com alteração para voltar ao menu principal
    Opção para voltar ao menu principal

--- Cartorio_EBAC.c
+++ Cartorio_EBAC.c
@@ -127,7 +127,8 @@ int main()
 	  printf("Escolha a opção desejada no menu:\n\n");
 	  printf("\t1 - Registrar nomes\n");
 	  printf("\t2 - Consultar nomes\n");
-	  printf("\t3 - Deletar nomes\n\n"); 
+	  printf("\t3 - Deletar nomes\n");
+	  printf("\t4 - Voltar para o menu principal\n\n");
 	  printf("Opção:"); //Fim do menu
 
       scanf("%d", &opcao); //Armazenando a escolha do usuário
@@ -148,6 +149,11 @@ int main()
 		case 3:
       	deletar();
    	    break;   
+   	    
+   	    case 4:
+   	    printf("Operação execultada com sucesso!");
+   	    return 0;
+   	    break;
    	    
 		default:
 		printf("Essa opção não está disponível\n");
